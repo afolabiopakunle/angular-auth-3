30d04fbfaf25ae9df97b226a9a3f333f10cb14cb
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-04-22 15:25:55 +0100

    all routes added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {EventsComponent} from "./events/events.component";
+import {PrivateEventsComponent} from "./private-events/private-events.component";
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'events', pathMatch: 'full'},
-  {path: 'events', component: EventsComponent}
+  {path: 'events', component: EventsComponent},
+  {path: 'private-events', component: PrivateEventsComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
