4560f21072fee83212e3a0f7c15d2e68bc471560
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-04-23 12:54:56 +0100

    error page implemented

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import {EventsComponent} from "./events/events.component";
 import {PrivateEventsComponent} from "./private-events/private-events.component";
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
+import {ErrorPageComponent} from "./error-page/error-page.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'events', pathMatch: 'full'},
   {path: 'events', component: EventsComponent},
   {path: 'private-events', component: PrivateEventsComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: '**', component: ErrorPageComponent}
 ];
 
 @NgModule({
