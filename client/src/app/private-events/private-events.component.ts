3ac33c0293f6c45038cc70139d7e71d04cd807f4
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-04-23 22:38:20 +0100

    event model added

--- client/src/app/private-events/private-events.component.ts
+++ client/src/app/private-events/private-events.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {EventService} from "../shared/event.service";
+import {EventModel} from "../shared/event.model";
 
 @Component({
   selector: 'app-private-events',
@@ -7,6 +8,8 @@ import {EventService} from "../shared/event.service";
   styleUrls: ['./private-events.component.css']
 })
 export class PrivateEventsComponent implements OnInit {
+
+  privateEvents: Object = [];
 
   constructor(private eventService: EventService) {
   }
@@ -18,8 +21,8 @@ export class PrivateEventsComponent implements OnInit {
   loadPrivateEvents() {
     this.eventService.getPrivateEvents()
       .subscribe(
-        res => {
-          console.log(res)
+        (res) => {
+          this.privateEvents = res;
         },
         err => {
           console.log(err)
---
+++ client/src/app/shared/event.model.ts
@@ -0,0 +1,5 @@
+export interface EventModel {
+  name: string,
+  date: string,
+  description: string
+}
